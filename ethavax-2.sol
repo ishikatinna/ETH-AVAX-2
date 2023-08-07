bf84c06de658e8b15b20928feb854712f1f4a399
ishikatinna <138487526+ishikatinna@users.noreply.github.com>
2023-08-07 23:08:46 +0530

    Update ethavax-2.sol

--- ethavax-2.sol
+++ ethavax-2.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract MyToken is ERC20 {
+    address private owner;
+    constructor() ERC20("MyToken", "MTK") {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the contract owner can call this function.");
+        _;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) public {
+        _burn(_msgSender(), amount);
+    }
+}
